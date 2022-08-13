cd78e24eb3547e956adee539b7597c5b6fb4066c
jimbrian <ui.ux.jimbrian@gmail.com>
2022-08-13 22:22:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 19;
+let num4 = 5;
+
+const multiply = num1 * num2;
+let random = Math.floor(Math.random()+1);
+let mod = num3 % num4;
+let max = Math.max(1,2,3,20);
+
+console.log(multiply, random, mod, max)
